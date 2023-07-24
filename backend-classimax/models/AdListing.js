9c919fcceeb159fa8289fc4670eb3ca92b944080
JyotiChetry <jyotichetry087@gmail.com>
2023-07-24 11:44:24 +0530

    setting up the backend server

---
+++ backend-classimax/models/AdListing.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const adListingSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User', //User model for sellers
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: [String],
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ['personal', 'business'],
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  negotiable: {
+    type: Boolean,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  payment_option: {
+    type: String,
+    enum: ['regular', 'top_featured', 'urgent'],
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('AdListing', adListingSchema);
